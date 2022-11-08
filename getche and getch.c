24e57f9ea9b47eef1ee49392b813e08e42e503c0
Animesh Acharya <animeshacharya867+github@gmail.com>
2022-11-08 20:54:43 +0545

    Added some new files

---
+++ getche and getch.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    char a,b;
+    printf("Enter a character:");
+    b=getche();
+    printf("Enter another character:");
+    a=getch();
+    printf("%c and %c",a,b);
+}
